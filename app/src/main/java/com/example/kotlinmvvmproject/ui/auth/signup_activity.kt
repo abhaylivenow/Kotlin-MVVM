4992701e095f1054058c94f44c75e54aec08f387
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2021-06-07 13:22:08 +0530

    ui and data package added

--- app/src/main/java/com/example/kotlinmvvmproject/ui/auth/signup_activity.kt
+++ app/src/main/java/com/example/kotlinmvvmproject/ui/auth/signup_activity.kt
@@ -1,11 +1,12 @@
-package com.example.kotlinmvvmproject
+package com.example.kotlinmvvmproject.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlinmvvmproject.R
 
-class MainActivity : AppCompatActivity() {
+class signup_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
