e8c77963d4346f9dcf199ad9dcd2674ed3a8124e
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2021-06-07 16:26:09 +0530

    ViewModels are connected with Login Activity

--- app/src/main/java/com/example/kotlinmvvmproject/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/kotlinmvvmproject/ui/auth/AuthViewModel.kt
@@ -7,12 +7,17 @@ class AuthViewModel : ViewModel(){
     var email : String? = null
     var password : String? = null
 
+    var authListener : AuthListener? = null
+
     fun onLoginButtonClicked(view : View){
+        authListener?.onStarted()
         if(email.isNullOrEmpty() || password.isNullOrEmpty()){
             // error message
+            authListener?.onFailure("Invalid email or password")
 
             return
         }
         // success
+        authListener?.onSuccess()
     }
 }
--- app/src/main/java/com/example/kotlinmvvmproject/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmvvmproject/ui/auth/LoginActivity.kt
@@ -2,11 +2,38 @@ package com.example.kotlinmvvmproject.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.kotlinmvvmproject.R
+import com.example.kotlinmvvmproject.databinding.ActivityLoginBinding
+import com.example.kotlinmvvmproject.util.toast
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(),AuthListener {
+    // to connect AuthViewModel to activity we need interface
+    // to bind the viewModel we need to remove setContentView(R.layout.activity_main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        // setContentView(R.layout.activity_login)
+        // ActivityLoginBinding class is automatic generated
+        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        val viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+        viewModel.authListener = this
+
+
+    }
+
+    override fun onStarted() {
+        // these toast are coming from extension function from viewUtil.kt class
+        toast("Login started")
+    }
+
+    override fun onSuccess() {
+        toast("Login success")
+    }
+
+    override fun onFailure(message: String) {
+        toast(message)
     }
 }
